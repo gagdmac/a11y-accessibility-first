81d52475e59efb6292eaa0750fbe562f0dc55b5e
garturogdiaz <garturogdiaz@gmail.com>
2024-11-14 23:46:37 +0100

    home links + trasnlate + info

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
   isSmallScreen = false;
 
   @HostListener('window:resize', ['$event'])
@@ -18,5 +21,9 @@ export class HomeComponent {
 
   checkScreenSize() {
     this.isSmallScreen = window.innerWidth < 1025;
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === '/' + path;
   }
 }
