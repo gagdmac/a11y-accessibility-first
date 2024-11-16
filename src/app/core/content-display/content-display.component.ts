f1ae6c8e3444e713c5344f51e02c2d2e7cf0671d
garturogdiaz <garturogdiaz@gmail.com>
2024-11-16 23:19:09 +0100

    content display component finished

--- src/app/core/content-display/content-display.component.ts
+++ src/app/core/content-display/content-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-content-display',
@@ -6,7 +7,34 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./content-display.component.scss'],
 })
 export class ContentDisplayComponent {
+  constructor(private router: Router) {}
+  //template condition to show the content
+  isSmallScreen = false;
+
   @Input() title: string = '';
-  @Input() content: string = '';
+  @Input() info: string = '';
+  @Input() extra: string = '';
+  @Input() more: string = '';
+  @Input() links: Array<{ text: string; route: string }> = [];
   @Input() ariaLabel?: string;
+  @Input() backgroundClass: string = '';
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth < 1025;
+  }
+
+  isActive(route: string): boolean {
+    return (
+      this.router.url === route || (route === 'home' && this.router.url === '/')
+    );
+  }
 }
