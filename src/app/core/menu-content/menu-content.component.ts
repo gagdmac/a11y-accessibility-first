93749d1b2aa4bd120199ffd2e2cc55d559d950f6
garturogdiaz <garturogdiaz@gmail.com>
2024-11-27 13:51:41 +0100

    slide menu in less than 1024

--- src/app/core/menu-content/menu-content.component.ts
+++ src/app/core/menu-content/menu-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -12,7 +12,14 @@ interface MenuItem {
   templateUrl: './menu-content.component.html',
   styleUrls: ['./menu-content.component.scss'],
 })
-export class MenuContentComponent {
+export class MenuContentComponent implements AfterViewInit {
+  screenWidth: number = window.innerWidth;
+  @ViewChild('slider') slider!: ElementRef;
+  canScrollLeft = false;
+  canScrollRight = false;
+  private touchStartX = 0;
+  private scrollStartX = 0;
+
   constructor(
     public router: Router,
     private translate: TranslateService
@@ -26,4 +33,43 @@ export class MenuContentComponent {
     { route: '/inclusivity', label: 'app-content-links.inclusivity' },
     { route: '/phisical', label: 'app-content-links.phisical' },
   ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  ngAfterViewInit() {
+    this.checkScrollButtons();
+  }
+
+  private checkScrollButtons() {
+    const el = this.slider.nativeElement;
+    this.canScrollLeft = el.scrollLeft > 0;
+    this.canScrollRight = el.scrollLeft < el.scrollWidth - el.clientWidth;
+  }
+
+  scroll(direction: 'left' | 'right') {
+    const el = this.slider.nativeElement;
+    const scrollAmount = el.clientWidth * 0.8;
+    el.scrollBy({
+      left: direction === 'left' ? -scrollAmount : scrollAmount,
+      behavior: 'smooth'
+    });
+    setTimeout(() => this.checkScrollButtons(), 100);
+  }
+
+  onTouchStart(e: TouchEvent) {
+    this.touchStartX = e.touches[0].pageX;
+    this.scrollStartX = this.slider.nativeElement.scrollLeft;
+  }
+
+  onTouchMove(e: TouchEvent) {
+    const touchDiff = this.touchStartX - e.touches[0].pageX;
+    this.slider.nativeElement.scrollLeft = this.scrollStartX + touchDiff;
+  }
+
+  onTouchEnd() {
+    this.checkScrollButtons();
+  }
 }
