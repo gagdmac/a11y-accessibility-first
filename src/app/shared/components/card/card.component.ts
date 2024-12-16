45cb5db279cc552d7bc96b3506291429bc782d93
garturogdiaz <garturogdiaz@gmail.com>
2024-12-16 19:47:32 +0100

    card disable conditional class added

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @ViewChild('cardElement') card!: ElementRef;
+
   @Input() title!: string;
   @Input() text!: string;
   @Input() link!: string;
   @Input() iconClass!: string;
   @Input() routerLink!: string;
+  @Input() isDisabled: boolean = false;
 }
